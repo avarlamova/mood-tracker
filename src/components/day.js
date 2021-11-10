8b2b5b896a4da7ffc826fe69dbe22bda3276c23e
avarlamova <59831804+avarlamova@users.noreply.github.com>
2021-11-10 23:34:19 +0300

    initial month component

---
+++ src/components/day.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class Day extends Component {
+  render() {
+    return <div>Another day...</div>;
+  }
+}
---
+++ src/components/month.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Day from "./Day";
+
+export default class month extends Component {
+  render() {
+    return <Day />;
+  }
+}
