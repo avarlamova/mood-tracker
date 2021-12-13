e431b0c8428343ae43bc9526b5b208bdee1af34c
avarlamova <59831804+avarlamova@users.noreply.github.com>
2021-12-13 22:47:27 +0300

    display year grid

--- src/components/day.js
+++ src/components/day.js
@@ -1,35 +1,34 @@
 import React, { Component } from "react";
+import moment from "moment";
+
+import "./day.css";
 
 export default class Day extends Component {
   state = {
-    counter: 1,
-    days: [],
+    daysCount: moment("2022-" + this.props.month, "YYYY-MM").daysInMonth(),
+    selectedDays: [],
   };
-
-  initializeDays = () => {
-    this.setState(() => {
-      let i = 1;
-      for (i; i < 32; i++) {
-        this.state.days.push(i);
-      }
-    });
-  };
-
-  сomponentDiDMount() {
-    console.log(this.state.days);
-
-    this.initializeDays();
-    console.log(this.state.days);
-  }
 
   render() {
+    let { month } = this.props;
+    if (month < 10) month = "0" + month;
+
+    const initialDays = Array(32)
+      .fill()
+      .map((x, i) => i)
+      .filter((i) => i > 0);
+
     return (
-      <div>
-        <ul>
-          {this.state.days.map((day, index) => (
-            <li key={index}>{day}</li>
-          ))}
-        </ul>
+      <div className="month-container">
+        {initialDays.map((day, index) => (
+          <div
+            id={"2022-" + month + "-" + (day > 9 ? day : "0" + day)}
+            key={index}
+            className="month-item"
+          >
+            {day}
+          </div>
+        ))}
       </div>
     );
   }
--- src/components/month.js
+++ src/components/month.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import Day from "./day.js";
+import "./month.css";
 
 export default class Month extends Component {
   render() {
+    const months = Array(13)
+      .fill()
+      .map((x, i) => i)
+      .filter((i) => i > 0);
+
     return (
       <div>
-        In another month...
-        <Day />
+        <div className="year-container">
+          {months.map((month, index) => (
+            <div key={index} className="year-item">
+              {" "}
+              {month}
+              <div>
+                <Day month={month} />{" "}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
