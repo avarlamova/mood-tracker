90aa4afb3d320f393e0bbbed4444c390444f7919
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-12-23 10:35:35 +0300

    refactor Year to TS component

---
+++ src/components/Year.tsx
@@ -0,0 +1,17 @@
+import Month from "./Month"
+
+const Year = () => {
+
+  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+  const renderedMonths = months.map((month, index) => {
+    return <Month key={index} monthNum={index+1} name={month}/>;
+  });
+    return (
+      <div>
+        Year 2023
+        {renderedMonths}
+      </div>
+    );
+}
+
+export default Year
